2735cb4cd5527afb24e417a83f55ceeb381c869b
tarangbhalodia <tarangbhalodia@hotmail.com>
2020-08-10 12:24:04 +0530

    geneate liquibase file

---
+++ src/main/java/com/school/management/config/LiquibaseConfiguration.java
@@ -0,0 +1,20 @@
+package com.school.management.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+  @Bean
+  public SpringLiquibase liquibase(ObjectProvider<DataSource> dataSource) {
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setChangeLog("classpath:db/liquibase/master.yaml");
+    liquibase.setDataSource(dataSource.getIfAvailable());
+    return liquibase;
+  }
+}
